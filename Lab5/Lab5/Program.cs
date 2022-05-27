a30cb692c2bf3c2da1d453bc67331cb305e90b79
Дмитро Шевченко <dima.shevchenko10@gmail.com>
2022-05-27 13:47:28 +0300

    4/8

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        private static void menu()
+        {
+            Console.WriteLine("1 - Ввести інформацію в масив з файлу");//+
+            Console.WriteLine("2 - Виконати лінійний пошук в масиві");//+
+            Console.WriteLine("3 - Виконати пошук з бар'єром");//+
+            Console.WriteLine("4 - Упорядкувати елементи масиву функцією сортування і виконати бінарний пошук");//+
+            Console.WriteLine("5 - Виконати прямий пошук підрядка");
+            Console.WriteLine("6 - Виконати алгоритм Кнута - Морріса - Пратта");//+
+            Console.WriteLine("7 - Виконати алгоритм Бойера - Мура");
+            Console.WriteLine("8 - Виконати алгоритм Рабина – Карпа");
+        }
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int [20];
+            int choice = -1;
+            int key;
+            string str;
+            string podstr;
+            while (true)
+            {
+                menu();
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        string[] lines = File.ReadAllLines("array.txt");
+                        arr = new int[lines.Length];
+                        int i = 0;
+                        foreach (var item in lines)
+                            arr[i++] = Convert.ToInt32(item);
+
+                        foreach (var item in arr)
+                            Console.Write(item + " ");
+
+                        Console.WriteLine("");
+                        break;
+
+                    case 2:
+                        key = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(search.LinearSearch(arr, key));
+                        break;
+
+                    case 3:
+                        key = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(search.LinearSearchBarrier(arr, key));
+                        break;
+
+                    case 4:
+                        BubbleSort.Sort(arr);
+                        key = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(search.BinarySearch(arr, key));
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Введите строку");
+                        str = Console.ReadLine();
+                        Console.WriteLine("Введите подстроку");
+                        podstr = Console.ReadLine();
+                        Console.WriteLine(str.IndexOf(podstr));
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Введите строку");
+                        str = Console.ReadLine();
+                        Console.WriteLine("Введите подстроку");
+                        podstr = Console.ReadLine();
+                        Console.WriteLine(FIndStr.FindSubstring(podstr,str));
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Введите строку");
+                        str = Console.ReadLine();
+                        Console.WriteLine("Введите подстроку");
+                        podstr = Console.ReadLine();
+                        Console.WriteLine(FIndStr.FindSubstring(podstr, str));
+                        break;
+                }
+
+            }
+        
+
+        }
+    }
+}
---
+++ Lab5/Lab5/search.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class search
+    {
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+        private static void CheckArray<T>(T[] a)
+        {
+            if (a == null)
+            {
+                throw new ArgumentNullException("Масив не може бути нульовим");
+            }
+
+            if (a.Length == 0)
+            {
+                throw new ArgumentException("Довжина масиву повинна бути більше нуля");
+            }
+        }
+        public static int LinearSearch(int[] a, int key)
+        {
+            CheckArray(a);
+
+            for (int i = 0; i < a.Length; ++i)
+            {
+                // порівнює поточне значення з ключем
+                if (a[i].Equals(key))
+                {
+                    return i;
+                }
+            }
+            //якщо нічого не знайдено
+            return -1;
+        }
+
+        public static int LinearSearchBarrier(int [] arr, int key)
+        {
+            int last = arr[arr.Length - 1];//Сохраним прежний элемент массива
+            arr[arr.Length - 1] = key;//Гарантируем, что value есть в массиве
+                                  //Есть гарантия того, что элемент есть в массиве, значит индекс можно не проверять
+            int i = 0;
+            for (i = 0; arr[i] != key; ++i)
+            {//Одно условие в цикле
+            }
+            arr[arr.Length - 1] = last;
+            if (i != (key - 1) || key == last)
+            {
+                return i;
+            }
+            return -1;
+        }
+
+    }
+}
